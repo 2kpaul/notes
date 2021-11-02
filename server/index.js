21efcc38b1557d9685205f5226f954846f832e52
Paul Maxineanu <paul.maxineanu@gmail.com>
2021-11-02 11:51:56 +0200

    add static folder and config app for deployment

--- server/index.js
+++ server/index.js
@@ -12,4 +12,13 @@ const notes = require('./routes/api/notes')
 
 app.use('/api/notes', notes)
 
+//handle production
+if(process.env.NODE_ENV === 'production') {
+    //set static folder for production
+    app.use(express.static(__dirname + '/public'))
+
+    //handle single page app
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'))
+}
+
 app.listen(port, () => console.log('server runnig on port ' + port))
